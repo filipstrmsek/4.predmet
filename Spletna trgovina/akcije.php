559b480fd8e9edf3246f5cddf40f174033aceca1
filipstrmsek <filip.strmsek@sers.si>
2026-03-29 16:25:51 +0200

    Add files via upload

---
+++ Spletna trgovina/akcije.php
@@ -0,0 +1,88 @@
+<?php include"Header.html" ?>
+
+<section class="products">
+        <h1>Akciska ponudba</h1>
+        <div class="product-list">
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€ <p class="znizana">xx€</p></p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <div class="product-image">Slika izdelka</div>
+                <div class="product-info">
+                    <p class="product-name">Ime izdelka</p>
+                    <p class="product-price">Cena: xx€</p>
+                    <button class="add-to-cart">Dodaj v košarico</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include"footer.html" ?>
---
+++ Spletna trgovina/vsekategorije.php
@@ -0,0 +1,7 @@
+<?php include"Header.html" ?>
+
+<h1 class="naslov">Vse kategorije</h1>
+
+<?php include"Kategorije.html" ?>
+
+<?php include"footer.html" ?>
